e96b6e39180129346f60f54065fcadc01eb6503f
lwl <liwulin@fmxos.com>
2018-07-03 21:04:17 +0800

    update

---
+++ springcloud-gateway/src/main/java/com/mm/GatewayApplication.java
@@ -0,0 +1,17 @@
+package com.mm;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
+
+/**
+ * @author lwl
+ * @date 2018/7/3
+ */
+@EnableDiscoveryClient
+@SpringBootApplication
+public class GatewayApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(GatewayApplication.class, args);
+    }
+}
